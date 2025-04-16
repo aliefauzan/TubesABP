f9ff08474aa267660785c0141a77e7bec41b1382
aliefauzan <afindo.mi01@gmail.com>
2025-04-16 23:27:32 +0800

    add new required data and dummy data

--- lib/models/booking.dart
+++ lib/models/booking.dart
@@ -8,6 +8,11 @@ class Booking {
   final String arrival;
   final String status;
   final String price;
+  final String passengerName;
+  final String passengerId;
+  final String passengerDob;
+  final String passengerGender;
+  final String seatClass;
 
   Booking({
     required this.transactionId,
@@ -19,5 +24,29 @@ class Booking {
     required this.arrival,
     required this.status,
     required this.price,
+    required this.passengerName,
+    required this.passengerId,
+    required this.passengerDob,
+    required this.passengerGender,
+    required this.seatClass,
   });
+
+  factory Booking.dummy() {
+    return Booking(
+      transactionId: '35151859256378',
+      trainName: 'Serayu 255',
+      operator: 'PT. KAI',
+      date: '16 Nov 2024',
+      time: '00:15',
+      departure: 'Kiaracondong',
+      arrival: 'Jatinegara',
+      status: 'Sudah dibayar',
+      price: 'Rp150.000',
+      passengerName: 'Alif Lohen',
+      passengerId: '77773023030043043',
+      passengerDob: '30 Mar 2003',
+      passengerGender: 'Laki-laki',
+      seatClass: 'Ekonomi',
+    );
+  }
 }
--- lib/models/train.dart
+++ lib/models/train.dart
@@ -1,23 +1,46 @@
 class Train {
+  final String id;
   final String name;
   final String operator;
   final String date;
   final String time;
   final String departure;
   final String arrival;
+  final String arrivalTime;
+  final String duration;
   final String classType;
   final String price;
   final int seatsLeft;
 
   Train({
+    required this.id,
     required this.name,
     required this.operator,
     required this.date,
     required this.time,
     required this.departure,
-    this.arrival = '',
+    required this.arrival,
+    required this.arrivalTime,
+    required this.duration,
     required this.classType,
     required this.price,
     required this.seatsLeft,
   });
+
+  factory Train.dummy() {
+    return Train(
+      id: '35151859256378',
+      name: 'Serayu 255',
+      operator: 'PT. KAI',
+      date: '16 Nov 2024',
+      time: '00:15',
+      departure: 'Kiaracondong',
+      arrival: 'Jatinegara',
+      arrivalTime: '03:55',
+      duration: '3h 40m',
+      classType: 'Ekonomi',
+      price: 'Rp150.000',
+      seatsLeft: 50,
+    );
+  }
 }
