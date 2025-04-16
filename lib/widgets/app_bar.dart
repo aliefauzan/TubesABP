8ffe1d0c5b3f29d19f71a9cec37768a6eee90db8
aliefauzan <afindo.mi01@gmail.com>
2025-04-16 23:29:08 +0800

    update widgets

--- lib/widgets/app_bar.dart
+++ lib/widgets/app_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:keretaxpress/utils/theme.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   const CustomAppBar({super.key});
@@ -6,62 +7,83 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      title: const Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text(
-            'KERETAXPRESS',
-            style: TextStyle(fontWeight: FontWeight.bold),
+          Image.asset(
+            'assets/logo/logo_white.png',
+            height: 40,
           ),
-          Text(
-            'AYO MAIK KERETA',
-            style: TextStyle(fontSize: 12),
+          const SizedBox(width: 8),
+          const Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'KERETAXPRESS',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                'AYO NAIK KERETA',
+                style: TextStyle(
+                  fontSize: 12,
+                  color: Colors.white,
+                ),
+              ),
+            ],
           ),
         ],
       ),
       actions: [
         IconButton(
-          icon: const Icon(Icons.notifications),
+          icon: const Icon(Icons.notifications, color: Colors.white),
           onPressed: () {},
         ),
       ],
-      // Update the bottom navigation in CustomAppBar
       bottom: PreferredSize(
-        preferredSize: const Size.fromHeight(40),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              TextButton(
-                onPressed: () {
-                  Navigator.pushNamedAndRemoveUntil(
-                    context, 
-                    '/', 
-                    (route) => false
-                  );
-                },
-                child: const Text('BERANDA'),
-              ),
-              TextButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/schedule');
-                },
-                child: const Text('JADWAL KEBERANGKATAN'),
-              ),
-              TextButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/schedule');
-                },
-                child: const Text('PESAN KERETA'),
-              ),
-              TextButton(
-                onPressed: () {
-                  // Add contact screen if needed
-                },
-                child: const Text('HUBUNGI KAMI'),
+        preferredSize: const Size.fromHeight(50),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.3),
+                spreadRadius: 1,
+                blurRadius: 5,
               ),
             ],
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                _NavButton(
+                  label: 'BERANDA',
+                  icon: Icons.home,
+                  onPressed: () => Navigator.pushNamedAndRemoveUntil(
+                    context, '/', (route) => false),
+                ),
+                _NavButton(
+                  label: 'JADWAL',
+                  icon: Icons.schedule,
+                  onPressed: () => Navigator.pushNamed(context, '/schedule'),
+                ),
+                _NavButton(
+                  label: 'PESAN',
+                  icon: Icons.train,
+                  onPressed: () => Navigator.pushNamed(context, '/schedule'),
+                ),
+                _NavButton(
+                  label: 'KONTAK',
+                  icon: Icons.contact_support,
+                  onPressed: () {},
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -69,5 +91,32 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   }
 
   @override
-  Size get preferredSize => const Size.fromHeight(100);
+  Size get preferredSize => const Size.fromHeight(110);
+}
+
+class _NavButton extends StatelessWidget {
+  final String label;
+  final IconData icon;
+  final VoidCallback onPressed;
+
+  const _NavButton({
+    required this.label,
+    required this.icon,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton.icon(
+      onPressed: onPressed,
+      icon: Icon(icon, color: AppTheme.primaryColor),
+      label: Text(
+        label,
+        style: const TextStyle(
+          color: AppTheme.textColor,
+          fontSize: 12,
+        ),
+      ),
+    );
+  }
 }
--- lib/widgets/train_card.dart
+++ lib/widgets/train_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import '../models/train.dart';
+import 'package:keretaxpress/models/train.dart';
+import 'package:keretaxpress/utils/theme.dart';
 
 class TrainCard extends StatelessWidget {
   final Train train;
@@ -14,8 +15,13 @@ class TrainCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      elevation: 2,
       margin: const EdgeInsets.only(bottom: 16),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12),
+      ),
       child: InkWell(
+        borderRadius: BorderRadius.circular(12),
         onTap: onTap,
         child: Padding(
           padding: const EdgeInsets.all(16.0),
@@ -25,31 +31,127 @@ class TrainCard extends StatelessWidget {
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text(
-                    '${train.name}',
-                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        train.name,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                      Text(
+                        train.operator,
+                        style: TextStyle(
+                          color: Colors.grey.shade600,
+                        ),
+                      ),
+                    ],
                   ),
-                  Text('Sisa kursi ${train.seatsLeft}'),
+                  Chip(
+                    backgroundColor: train.seatsLeft > 10
+                        ? Colors.green.shade100
+                        : Colors.orange.shade100,
+                    label: Text(
+                      '${train.seatsLeft} kursi tersedia',
+                      style: TextStyle(
+                        color: train.seatsLeft > 10 
+                            ? Colors.green 
+                            : Colors.orange,
+                      ),
+                    ),
+                  ),
                 ],
               ),
-              Text(train.operator),
-              const SizedBox(height: 10),
+              const SizedBox(height: 16),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(train.date),
-                      Text(train.time),
-                      Text(train.departure),
+                      Text(
+                        train.departure,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      Text(
+                        train.time,
+                        style: const TextStyle(
+                          fontSize: 14,
+                        ),
+                      ),
                     ],
                   ),
                   Column(
                     children: [
-                      Text(train.classType),
-                      Text(train.price),
+                      Container(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 8,
+                          vertical: 4,
+                        ),
+                        decoration: BoxDecoration(
+                          color: AppTheme.secondaryColor.withOpacity(0.1),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Text(
+                          train.classType,
+                          style: TextStyle(
+                            color: AppTheme.secondaryColor,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 4),
+                      Text(
+                        train.duration,
+                        style: TextStyle(
+                          color: Colors.grey.shade600,
+                          fontSize: 12,
+                        ),
+                      ),
                     ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        train.arrival,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      Text(
+                        train.arrivalTime,
+                        style: const TextStyle(
+                          fontSize: 14,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    train.date,
+                    style: TextStyle(
+                      color: Colors.grey.shade600,
+                    ),
+                  ),
+                  Text(
+                    train.price,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                      color: AppTheme.primaryColor,
+                    ),
                   ),
                 ],
               ),
